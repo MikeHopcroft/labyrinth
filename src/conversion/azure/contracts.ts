1dfe47ffedbc3ff42989c8359ecfa32c102e9ccd
Eric Maino <ericmai@microsoft.com>
2021-02-23 14:31:08 -0800

    DOC: Adding design note on Azure converter

--- src/conversion/azure/contracts.ts
+++ src/conversion/azure/contracts.ts
@@ -9,6 +9,11 @@ export interface ItemMoniker {
   readonly name: string;
 }
 
+// DESIGN NOTE: Both monikers and convert return an array or list
+// of items as it is possible that a top level item in the Azure
+// graph consists of internal elements which may be referenced by
+// other items in teh graph. An example of this could be network
+// interface card on a Virutal Machine in a scale set
 export interface IAzureConverter<T extends AnyAzureObject> {
   readonly supportedType: string;
   monikers(input: T): ItemMoniker[];
