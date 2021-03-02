636424c1fc59b56fd4612e799fc1d0ec29ce0e27
Eric Maino <ericmai@microsoft.com>
2021-03-02 14:43:04 -0800

    REFACTOR: Implement NSG Converter

--- src/conversion/azure2/convert_subnet.ts
+++ src/conversion/azure2/convert_subnet.ts
@@ -1,7 +1,12 @@
-import {RoutingRuleSpec} from '../../graph';
+import {NodeSpec, RoutingRuleSpec} from '../../graph';
+import {IRules} from '../types';
 
 import {GraphServices} from './graph_services';
-import {AzureSubnet} from './types';
+import {
+  AzureIPConfiguration,
+  AzureNetworkSecurityGroup,
+  AzureSubnet,
+} from './types';
 
 export function convertSubnet(
   services: GraphServices,
@@ -20,7 +25,9 @@ export function convertSubnet(
   const rules: RoutingRuleSpec[] = [
     // Traffic leaving subnet
     {
-      constraints: {destinationIp: `except ${subnetSpec.properties.addressPrefix}`},
+      constraints: {
+        destinationIp: `except ${subnetSpec.properties.addressPrefix}`,
+      },
       destination: outboundKey,
     },
   ];
@@ -28,14 +35,64 @@ export function convertSubnet(
   // For each ipConfiguration
   //   Materialize ipConfiguration
   //   Add routing rule
+  if (subnetSpec.properties.ipConfigurations) {
+    for (const ip of subnetSpec.properties.ipConfigurations) {
+      // Subnets may have ip configurations attached for items which do not exist in the
+      // the resource graph. The first example of this is specifically for Virtual Machine
+      // Scale Set ip configurations.
+      if (services.hasItem(ip.id)) {
+        const ipConfig = services.dereference<AzureIPConfiguration>(ip);
+        const ipServiceTag = services.convert.ip(services, ipConfig);
+        rules.push({
+          destination: ipServiceTag,
+          constraints: {destinationIp: ipServiceTag},
+        });
+      }
+    }
+  }
 
   const routerNode: NodeSpec = {
     key: routerKey,
     range: {
-      sourceIp: subnet.properties.addressPrefix,
+      sourceIp: subnetSpec.properties.addressPrefix,
     },
     routes: rules,
   };
-  nodes.push(routerNode);
-  return 'subnet';
+  services.addNode(routerNode);
+
+  let nsgRules: IRules | null = null;
+  if (subnetSpec.properties.networkSecurityGroup) {
+    const nsgSpec = services.dereference<AzureNetworkSecurityGroup>(
+      subnetSpec.properties.networkSecurityGroup
+    );
+
+    // FIX: vNetKey needs to be a symbol not just a node key
+    nsgRules = services.convert.nsg(services, nsgSpec, vNetKey);
+  }
+
+  const inboundNode: NodeSpec = {
+    key: inboundKey,
+    filters: nsgRules?.inboundRules ?? [],
+    // NOTE: no range because inbound can receive from any sourceIp
+    // TODO: is this correct? The router moves packets in both directions.
+    routes: [
+      {
+        destination: routerKey,
+      },
+    ],
+  };
+  services.addNode(inboundNode);
+
+  const outboundNode: NodeSpec = {
+    key: outboundKey,
+    filters: nsgRules?.outboundRules ?? [],
+    range: {
+      sourceIp: subnetSpec.properties.addressPrefix,
+    },
+    routes: [],
+  };
+  services.addNode(outboundNode);
+
+  // TODO: What should be returned here? Subnet is represetned by 3 nodes...
+  return inboundNode.key;
 }
