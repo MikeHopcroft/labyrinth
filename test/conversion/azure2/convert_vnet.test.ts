df6114746c18b894c796a1d759f5ce13635b3ed2
Eric Maino <ericmai@microsoft.com>
2021-03-02 20:20:57 -0800

    WIP: Sample VNET Test

---
+++ test/conversion/azure2/convert_vnet.test.ts
@@ -0,0 +1,100 @@
+import {assert} from 'chai';
+import 'mocha';
+import {ActionType, GraphSpec} from '../../../src';
+
+import {ServiceOracle} from './oracle';
+import {ResourceGraphOracle} from './oracle_data';
+
+describe('Azure', () => {
+  describe('Convert-VNET', () => {
+    it('Basic Virtual Network Conversion', () => {
+      const expected = {
+        nodes: [
+          {
+            key:
+              '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B/subnets/A/router',
+            routes: [
+              {
+                constraints: {destinationIp: 'except 172.18.0.0/28'},
+                destination:
+                  '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B/subnets/A/outbound',
+              },
+            ],
+          },
+          {
+            key:
+              '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B/subnets/A/inbound',
+            filters: [
+              {
+                action: ActionType.ALLOW,
+                priority: 65000,
+                id: 1,
+                source: 'data/azure/resource-graph-1.json',
+                constraints: {
+                  sourceIp:
+                    '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B',
+                  sourcePort: '*',
+                  destinationIp:
+                    '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B',
+                  destinationPort: '*',
+                  protocol: '*',
+                },
+              },
+            ],
+            routes: [
+              {
+                destination:
+                  '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B/subnets/A/router',
+              },
+            ],
+          },
+          {
+            key:
+              '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B/subnets/A/outbound',
+            filters: [],
+            routes: [
+              {
+                destination:
+                  '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B',
+              },
+            ],
+          },
+          {
+            key:
+              '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B',
+            range: {sourceIp: '172.18.0.0/28'},
+            routes: [
+              {
+                destination: 'Internet',
+                constraints: {destinationIp: 'except 172.18.0.0/28'},
+              },
+              {
+                destination:
+                  '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B/subnets/A/inbound',
+                constraints: {
+                  destinationIp:
+                    '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B/subnets/A/inbound',
+                },
+              },
+            ],
+          },
+        ],
+        symbols: [
+          {
+            dimension: 'ip',
+            symbol:
+              '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testing-network-testing/providers/Microsoft.Network/virtualNetworks/VNET-B',
+            range: '172.18.0.0/28',
+          },
+        ],
+      };
+
+      const vnetSpec = ResourceGraphOracle.ValidVnet();
+      const graph = ResourceGraphOracle.ValidVnetGraph();
+      const services = ServiceOracle.InitializedGraphServices(graph);
+
+      services.convert.vnet(services, vnetSpec);
+      assert.deepEqual(services.getLabyrinthGraphSpec(), expected);
+    });
+  });
+});
--- test/conversion/azure2/oracle.ts
+++ test/conversion/azure2/oracle.ts
@@ -2,12 +2,15 @@ import {AzureObjectIndex} from '../../../src/conversion/azure2/azure_object_inde
 import {DefaultConverterConfig} from '../../../src/conversion/azure2/convert';
 import {GraphServices} from '../../../src/conversion/azure2/graph_services';
 import {SymbolTable} from '../../../src/conversion/azure2/symbol_table';
+import {AzureResourceGraph} from '../../../src/conversion/azure2/types';
 
 export class ServiceOracle {
-  static InitializedGraphServices(): GraphServices {
+  static InitializedGraphServices(
+    spec: AzureResourceGraph = []
+  ): GraphServices {
     const converters = DefaultConverterConfig;
     const symbolTable = new SymbolTable([]);
-    const index = new AzureObjectIndex([]);
+    const index = new AzureObjectIndex(spec);
     return new GraphServices(converters, symbolTable, index);
   }
 }
