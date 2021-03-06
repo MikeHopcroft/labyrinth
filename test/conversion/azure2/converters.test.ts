36c22255618fe27c215e1d6b7d74e8d686a3345c
Michael Hopcroft <mhop@microsoft.com>
2021-03-05 17:03:11 -0800

    TEST: convert_subnet.test.ts

--- test/conversion/azure2/converters.test.ts
+++ test/conversion/azure2/converters.test.ts
@@ -1,11 +1,13 @@
 import 'mocha';
 
 import ip from './convert_ip.test';
+import subnet from './convert_subnet.test';
 import vnet from './convert_vnet.test';
 
 export default function test() {
   describe('Converters', () => {
     ip();
+    subnet();
     vnet();
   });
 }
