2a6d4a162c29ff11a5761cdf5fdeb97f5e7c9f23
Eric Maino <ericmai@microsoft.com>
2021-03-20 17:30:17 -0700

    REFACTOR: Move createNicRoutes into separate function

--- src/conversion/azure/convert_public_ip.ts
+++ src/conversion/azure/convert_public_ip.ts
@@ -14,38 +14,47 @@ export function convertPublicIp(
   gatewayKey: string,
   internetKey: string
 ): PublicIpRoutes {
-  if (!spec.properties.ipConfiguration) {
-    return {inbound: undefined, outbound: undefined};
+  if (spec.properties.ipConfiguration) {
+    const ipconfig = services.index.dereference(
+      spec.properties.ipConfiguration
+    );
+
+    if (ipconfig.type === AzureObjectType.PRIVATE_IP) {
+      return createNicRoutesForPublicIp(
+        spec.properties.ipAddress,
+        ipconfig.properties.privateIPAddress,
+        internetKey,
+        gatewayKey
+      );
+    }
   }
 
-  const ipconfig = services.index.dereference(spec.properties.ipConfiguration);
+  return {inbound: undefined, outbound: undefined};
+}
 
-  let privateIp: string | undefined;
-  let outbound: RoutingRuleSpec | undefined;
-
-  if (ipconfig.type === AzureObjectType.PRIVATE_IP) {
-    privateIp = ipconfig.properties.privateIPAddress;
-
-    outbound = {
-      destination: internetKey,
-      constraints: {
-        sourceIp: privateIp,
-      },
-      override: {
-        sourceIp: spec.properties.ipAddress,
-      },
-    };
-  } else {
-    return {inbound: undefined, outbound: undefined};
-  }
-
+function createNicRoutesForPublicIp(
+  publicIp: string,
+  privateIp: string,
+  internetKey: string,
+  gatewayKey: string
+): PublicIpRoutes {
   const inbound: RoutingRuleSpec = {
     destination: gatewayKey,
     constraints: {
-      destinationIp: spec.properties.ipAddress,
+      destinationIp: privateIp,
     },
     override: {
       destinationIp: privateIp,
+    },
+  };
+
+  const outbound = {
+    destination: internetKey,
+    constraints: {
+      sourceIp: privateIp,
+    },
+    override: {
+      sourceIp: publicIp,
     },
   };
 
