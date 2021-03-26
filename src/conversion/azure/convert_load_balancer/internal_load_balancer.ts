a9a2de5f7bd9d86396a8d78d363a0cf2c36d67ba
Eric Maino <ericmai@microsoft.com>
2021-03-26 14:54:17 -0700

    REFACTOR: Move createLoadBalancerRoutes

--- src/conversion/azure/convert_load_balancer/internal_load_balancer.ts
+++ src/conversion/azure/convert_load_balancer/internal_load_balancer.ts
@@ -1,6 +1,14 @@
 import {RoutingRuleSpec, SimpleRoutingRuleSpec} from '../../../graph';
 
-import {AzureLoadBalancer} from '../azure_types';
+import {
+  AzureLoadBalancer,
+  AzureLoadBalancerBackendPool,
+  AzureLoadBalancerFrontEndIp,
+  AzureLoadBalancerInboundNatRule,
+  AzureLoadBalancerInboundRule,
+  AzureLoadBalancerRule,
+  AzurePrivateIP,
+} from '../azure_types';
 import {GraphServices} from '../graph_services';
 
 export function convertInternalLoadBalancer(
@@ -45,4 +53,81 @@ export function convertInternalLoadBalancer(
 export function isInternalLoadBalancer(spec: AzureLoadBalancer): boolean {
   const ipConfig = spec.properties.frontendIPConfigurations[0];
   return (ipConfig && ipConfig.properties.privateIPAddress) !== undefined;
+}
+
+export function createLoadBalancerRoutes(
+  services: GraphServices,
+  spec: AzureLoadBalancerFrontEndIp,
+  backboneKey: string
+): RoutingRuleSpec[] {
+  services.nodes.markTypeAsUsed(spec);
+  services.nodes.markTypeAsUsed(spec);
+
+  const routes: RoutingRuleSpec[] = [];
+
+  for (const lbRuleRef of spec.properties.loadBalancingRules ?? []) {
+    const lbRule = services.index.dereference<AzureLoadBalancerInboundRule>(
+      lbRuleRef
+    );
+    services.nodes.markTypeAsUsed(lbRule);
+
+    const backendPool = services.index.dereference<AzureLoadBalancerBackendPool>(
+      lbRule.properties.backendAddressPool
+    );
+    services.nodes.markTypeAsUsed(backendPool);
+
+    const backendIPs = backendPool.properties.backendIPConfigurations.map(
+      ip =>
+        services.index.dereference<AzurePrivateIP>(ip).properties
+          .privateIPAddress
+    );
+
+    routes.push(createInboundRoute(lbRule, backboneKey, ...backendIPs));
+  }
+
+  for (const natRuleSpec of spec.properties.inboundNatRules ?? []) {
+    const natRule = services.index.dereference<AzureLoadBalancerInboundNatRule>(
+      natRuleSpec
+    );
+    services.nodes.markTypeAsUsed(natRule);
+
+    const backendIp = services.index.dereference<AzurePrivateIP>(
+      natRule.properties.backendIPConfiguration
+    );
+    services.nodes.markTypeAsUsed(backendIp);
+
+    routes.push(
+      createInboundRoute(
+        natRule,
+        backboneKey,
+        backendIp.properties.privateIPAddress
+      )
+    );
+  }
+  return routes;
+}
+
+function createInboundRoute(
+  spec: AzureLoadBalancerRule,
+  subnetKey: string,
+  ...backendIps: string[]
+): RoutingRuleSpec {
+  const rule = spec.properties;
+
+  const ruleSpec: RoutingRuleSpec = {
+    destination: subnetKey,
+    constraints: {
+      destinationPort: rule.frontendPort.toString(),
+      protocol: rule.protocol,
+    },
+    override: {
+      destinationIp: backendIps.join(','),
+    },
+  };
+
+  if (rule.backendPort !== rule.frontendPort) {
+    ruleSpec.override!.destinationPort = rule.backendPort.toString();
+  }
+
+  return ruleSpec;
 }
--- src/conversion/azure/convert_load_balancer/load_balancer_front_end_ip.ts
+++ src/conversion/azure/convert_load_balancer/load_balancer_front_end_ip.ts
@@ -1,15 +1,9 @@
 import {RoutingRuleSpec} from '../../../graph';
 
-import {
-  AzureLoadBalancerBackendPool,
-  AzureLoadBalancerFrontEndIp,
-  AzureLoadBalancerInboundNatRule,
-  AzureLoadBalancerInboundRule,
-  AzureLoadBalancerRule,
-  AzurePrivateIP,
-} from '../azure_types';
-
+import {AzureLoadBalancerFrontEndIp} from '../azure_types';
 import {GraphServices} from '../graph_services';
+
+import {createLoadBalancerRoutes} from './internal_load_balancer';
 
 export function convertLoadBalancerFrontEndIp(
   services: GraphServices,
@@ -20,81 +14,4 @@ export function convertLoadBalancerFrontEndIp(
   const routes = createLoadBalancerRoutes(services, spec, backboneKey);
   services.nodes.add({key, routes});
   return {destination: key};
-}
-
-export function createLoadBalancerRoutes(
-  services: GraphServices,
-  spec: AzureLoadBalancerFrontEndIp,
-  backboneKey: string
-): RoutingRuleSpec[] {
-  services.nodes.markTypeAsUsed(spec);
-  services.nodes.markTypeAsUsed(spec);
-
-  const routes: RoutingRuleSpec[] = [];
-
-  for (const lbRuleRef of spec.properties.loadBalancingRules ?? []) {
-    const lbRule = services.index.dereference<AzureLoadBalancerInboundRule>(
-      lbRuleRef
-    );
-    services.nodes.markTypeAsUsed(lbRule);
-
-    const backendPool = services.index.dereference<AzureLoadBalancerBackendPool>(
-      lbRule.properties.backendAddressPool
-    );
-    services.nodes.markTypeAsUsed(backendPool);
-
-    const backendIPs = backendPool.properties.backendIPConfigurations.map(
-      ip =>
-        services.index.dereference<AzurePrivateIP>(ip).properties
-          .privateIPAddress
-    );
-
-    routes.push(createInboundRoute(lbRule, backboneKey, ...backendIPs));
-  }
-
-  for (const natRuleSpec of spec.properties.inboundNatRules ?? []) {
-    const natRule = services.index.dereference<AzureLoadBalancerInboundNatRule>(
-      natRuleSpec
-    );
-    services.nodes.markTypeAsUsed(natRule);
-
-    const backendIp = services.index.dereference<AzurePrivateIP>(
-      natRule.properties.backendIPConfiguration
-    );
-    services.nodes.markTypeAsUsed(backendIp);
-
-    routes.push(
-      createInboundRoute(
-        natRule,
-        backboneKey,
-        backendIp.properties.privateIPAddress
-      )
-    );
-  }
-  return routes;
-}
-
-function createInboundRoute(
-  spec: AzureLoadBalancerRule,
-  subnetKey: string,
-  ...backendIps: string[]
-): RoutingRuleSpec {
-  const rule = spec.properties;
-
-  const ruleSpec: RoutingRuleSpec = {
-    destination: subnetKey,
-    constraints: {
-      destinationPort: rule.frontendPort.toString(),
-      protocol: rule.protocol,
-    },
-    override: {
-      destinationIp: backendIps.join(','),
-    },
-  };
-
-  if (rule.backendPort !== rule.frontendPort) {
-    ruleSpec.override!.destinationPort = rule.backendPort.toString();
-  }
-
-  return ruleSpec;
 }
