90d2c3d88e8316e2ad655dd173335847c83fb5d0
Eric Maino <ericmai@microsoft.com>
2021-04-02 13:50:17 -0700

    FIX: Address an issue where some references are typed

--- src/conversion/azure/walk.ts
+++ src/conversion/azure/walk.ts
@@ -16,9 +16,35 @@ export function* walkAzureTypedObjects(
     const resourceGroup = root.resourceGroup;
     const type = root.type;
 
-    if (id && name && resourceGroup && type) {
+    // DESIGN NOTE: It seems that while most references only have id and name
+    // it's possible Azure has some old classic references in which the reference
+    // is strongly typeed. One case here is that properties could be looked for
+    // however it's not determined that every object will have properties. This
+    // implementation decided to check that all 4 properties are set and the object
+    // has at least one or more additional properties
+    // {
+    //   id: '',
+    //   type: 'microsoft.logic/workflows',
+    //   resourceGroup: '',
+    //   name: '',
+    //   properties: {
+    //     sku: {
+    //       name: 'Standard',
+    //       plan: {
+    //         id: '',
+    //         name: 'Default1',
+    //         resourceGroup: 'Default-Web-EastUS',
+    //         type: 'Microsoft.Web/ServerFarms',
+    //       },
+    //     },
+    //   },
+    // };
+    const keyCount = Object.keys(root).length;
+
+    if (id && name && resourceGroup && type && keyCount > 4) {
       yield root;
     }
+
     for (const key in root) {
       // https://eslint.org/docs/rules/no-prototype-builtins
       if (!Object.prototype.hasOwnProperty.call(root, key)) {
